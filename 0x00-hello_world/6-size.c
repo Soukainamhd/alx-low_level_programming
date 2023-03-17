dc1fbdd6555ed1904ab5065dd58e1937f389d3d8
soukaina <soukmahdar@gmail.com>
2023-03-17 14:55:59 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	printf("size of a char: %lu byte(s)", sizeof(char));
 	printf("size of a int: %lu byte(s)", sizeof(int));
 	printf("size of a long int: %lu byte(s)", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)",sizeof(lon long int));
+	printf("size of a long long int: %lu byte(s)", sizeof(long int));
 	printf("size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
